42760c37e600989bf66101c81109ec61c90c0751
LIMachi <lee.mach.hm@gmail.com>
2019-03-01 15:23:29 +0100

    added installation of cmake

--- default_var.mk
+++ default_var.mk
@@ -16,8 +16,13 @@ PACKAGE_MANAGER_BIN = $(HOME)/.brew/bin
 PACKAGE_MANAGER_LIB = $(HOME)/.brew/lib
 PACKAGE_MANAGER_INC = $(HOME)/.brew/include
 
+CMAKE = $(PACKAGE_MANAGER_BIN)/cmake
+
 $(PACKAGE_MANAGER):
 	git clone --depth=1 https://github.com/Homebrew/brew $(PACKAGE_MANAGER_DIR) && echo 'export PATH=$(PACKAGE_MANAGER_BIN):$(PATH)' >> $(HOME)/.zshrc && source $(HOME)/.zshrc && brew update
+
+$(CMAKE):
+	$(PACKAGE_MANAGER) install cmake
 
 else
 
