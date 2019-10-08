e89825f91cdcbdedc6a04d64efead6c808ed2828
LIMachi <lee.mach.hm@gmail.com>
2019-10-08 05:09:30 +0200

    added default includes path to CMake rule for compatibility with Mac, because mac does not like /usr/include

--- common.mk
+++ common.mk
@@ -109,14 +109,16 @@ else
 	@find . -type f | grep "\.c$$" $(foreach V, $(BLACK_LIST_SRCS), | grep -v "$(V)") $(foreach V, $(TEST_SRCS), | grep -v "^$(V)$$") | cut -f2- -d/ | grep -v " " | sed "s/^/       /" | sed "s/$$/ \\\\/" | sed "1s/^       //" | sed "$$ s/..$$//" >> $(MAKEFILE_FILES_DIR)/srcs
 endif
 
+CMAKE_INCLUDE_DIRECTORIES := $(shell echo | clang -Wp,-v -xc - 2>&1 | grep " /" | cut -c2- | grep -v " ")
+
 CMake:
 ifneq ($(VERBOSE), )
 	@echo $(LOCAL_MAKEFILE): preparing CMakeLists.txt
-	echo "cmake_minimum_required(VERSION 3.12)\nproject($(NAME))\nset(CMAKE_CXX_STANDARD 14)\ninclude_directories($(INC_DIR))" > CMakeLists.txt
+	echo "cmake_minimum_required(VERSION 3.12)\nproject($(NAME))\nset(CMAKE_CXX_STANDARD 14)\ninclude_directories($(INC_DIR) $(CMAKE_INCLUDE_DIRECTORIES))" > CMakeLists.txt
 	$(foreach V, $(CLIB), echo "add_subdirectory($(dir $(V)) $(MAKEFILE_FILES_DIR))" >> CMakeLists.txt;)
 	echo "add_library($(NAME) $(TEST_SRCS) $(SRCS))" >> CMakeLists.txt
 else
-	@echo "cmake_minimum_required(VERSION 3.12)\nproject($(NAME))\nset(CMAKE_CXX_STANDARD 14)\ninclude_directories($(INC_DIR))" > CMakeLists.txt
+	@echo "cmake_minimum_required(VERSION 3.12)\nproject($(NAME))\nset(CMAKE_CXX_STANDARD 14)\ninclude_directories($(INC_DIR) $(CMAKE_INCLUDE_DIRECTORIES))" > CMakeLists.txt
 	@$(foreach V, $(CLIB), echo "add_subdirectory($(dir $(V)) $(MAKEFILE_FILES_DIR))" >> CMakeLists.txt;)
 	@echo "add_library($(NAME) $(TEST_SRCS) $(SRCS))" >> CMakeLists.txt
 endif
